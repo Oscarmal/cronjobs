444182204a38a5028bca75c6e90b9d2d0f8a7e68
Oscar Maldonado <oscar.maldonado@isolution.mx>
2015-04-15 15:48:07 -0500

    Se reordena codigo

--- cronjobs.php
+++ cronjobs.php
@@ -6,29 +6,13 @@
 * Modificación:	2015-04-15
 */
 require_once("libs/o3m.functions.php");
-extract($_GET, EXTR_PREFIX_ALL, "v");
-extract($_POST, EXTR_PREFIX_ALL, "v"); 
-
 // Parametros
 $parms[log_txt] = true; # Activar logs en archivos txt
 $parms[log_db]  = true; # Activar logs en base de datos
 $parms[cron_paths]  = 'cron/'; # Ruta de los archivos cronjob
-
 // Tiempo de inicio del script
 $t_entrada = date("Y-m-d H:i:s");
-
 // 
-switch (strtolower($v_a)) {
-	case 'tbl':
-		echo "<h3>Cronjobs: ".$t_entrada.'</h3>'."<hr/>";
-		echo table_cron_tareas();
-		break;
-	case 'exe':
-		execute_cron_tareas();
-		break;	
-	default:
-		echo utf8_decode("Sin autorización");
-		break;
-}
+execute_cron_tareas();
 /*O3M*/
 ?>
