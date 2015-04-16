0f42b37af4de00bde6f253054e009ef49a628617
Oscar Maldonado <oscar.maldonado@isolution.mx>
2015-04-16 18:23:40 -0500

    Se devuelve respuesta

--- cron/test/crea.txt.php
+++ cron/test/crea.txt.php
@@ -5,7 +5,8 @@ function crea_txt($contenido=''){
 	$file=fopen(date("Ymd-His").'.txt',"a") or die("No se creó el archivo");
   	fputs($file,$contenido);
   	fclose($file);
+  	return "TXT creado OK: ".date("Y-m-d H:i:s");
 }
-crea_txt("Creado el: ".date("Y-m-d H:i:s"));
+echo crea_txt("Creado el: ".date("Y-m-d H:i:s"));
 
 ?>
