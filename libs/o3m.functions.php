8a983d92e4de6bdf65909f26eebfa610d0d0f411
Oscar Maldonado <oscar.maldonado@isolution.mx>
2015-04-16 17:49:38 -0500

    Se corrige query de cron_log

--- libs/o3m.functions.php
+++ libs/o3m.functions.php
@@ -22,6 +22,56 @@ function dump_var($variable,$tipo=0){
 #Funciones Negocio
 ##################
 
+function table_cronjob_estatus(){
+// Muestra tabla con cronjobs activos en la tabla cron_tareas
+	$datos = sql_select_verifica_cron();
+	$tbl_resultados .= '<table border="1">';
+	$tbl_resultados .= '
+		<thead>
+			<th>id_cron_log</th>
+			<th>id_cronjob</th>
+			<th>cron_nombre</th>
+			<th>ejecuta</th>
+			<th>inicio_fecha</th>
+			<th>inicio_hora</th>
+			<th>inicio_tiempo</th>
+			<th>fin_fecha</th>
+			<th>fin_hora</th>
+			<th>fin_tiempo</th>
+			<th>ejecucion_tipo</th>
+			<th>ejecucion_valor</th>
+			<th>ejecucion_segundos</th>
+			<th>ejecucion_minutos</th>
+			<th>ejecucion_tiempo</th>
+			<th>estatus</th>
+			<th>inicio</th>
+			<th>fin</th>
+			<th>respuesta</th>
+			<th>timestamp</th>
+			<th>proxima_ejecucion</th>
+			<th>ahora</th>
+			<th>excedido_tiempo</th>
+			<th>excedido_minutos</th>
+			<th>cron_iniciado</th>
+			<th>cron_vigente</th>
+			<th>cron_activo</th>
+		</thead>';
+	foreach($datos as $registro){
+		$tbl_resultados .= '<tr>';
+		$soloUno = (!is_array($registro))?true:false; 
+		$data = (!$soloUno)?$registro:$datos; 
+		// Dibuja tabla
+		for($i=0; $i<count($data)/2; $i++){
+			$tbl_resultados .= '<td>'.$data[$i].'</td>';
+		}	
+		if($soloUno) break;
+		$tbl_resultados .= '</tr>';				
+	}
+	$tbl_resultados .= '</table>';
+	$tbl_resultados .= '<a href="#" onclick="location.reload();">Actualizar</a>';
+	return $tbl_resultados;
+}
+
 function table_cron_tareas(){
 // Muestra tabla con cronjobs activos en la tabla cron_tareas
 	$datos = sql_select_cron_tareas();
@@ -51,7 +101,6 @@ function table_cron_tareas(){
 		for($i=0; $i<count($data)/2; $i++){
 			$tbl_resultados .= '<td>'.$data[$i].'</td>';
 		}
-		// $exe_cron = execute_cronjob($data[0]);	
 		if($soloUno) break;
 		$tbl_resultados .= '</tr>';				
 	}
@@ -134,7 +183,7 @@ function logs_txt($contenido=''){
 	global $parms;
 	$ruta = $parms[root_path].'logs/';
 	$contenido = date("Y-m-d H:i:s").' -> '.$contenido."\r\n";
-	$file=fopen($ruta."logs_cronjobs_".date("Ymd").".txt","a+") or die("No se creó el archivo");
+	$file=fopen($ruta."logs_cronjobs_".date("Ymd").".log","a+") or die("No se creó el archivo");
   	fputs($file,$contenido);
   	fputs($file,"-------\r\n");
   	fclose($file);
@@ -197,10 +246,10 @@ function sql_update_cron_logs($data=array()){
 function sql_select_verifica_cron($data=array()){
 // Listado de tabla cron_tareas
 	$id_cronjob = $data[id_cronjob];
-	$filtro .= ($id_cronjob)?" AND a.id_cronjob='$id_cronjob'":'';
+	$filtro .= ($id_cronjob)?" AND b.id_cronjob='$id_cronjob'":'';
 	$sql = "SELECT 
 				 a.id_cron_log
-				,a.id_cronjob
+				,b.id_cronjob
 				,b.cron_nombre
 				,b.ejecuta
 				,b.inicio_fecha
@@ -221,11 +270,11 @@ function sql_select_verifica_cron($data=array()){
 				+IF(b.ejecucion_tipo='HORAS',IFNULL(b.ejecucion_valor,0)*60*60,0) 
 				+IF(b.ejecucion_tipo='DIAS',IFNULL(b.ejecucion_valor,0)*60*60*24,0)),'%H:%i') AS ejecucion_tiempo
 				,a.estatus
-				,a.inicio
-				,a.fin
+				,IFNULL(a.inicio,NOW()) as inicio
+				,IFNULL(a.fin,NOW()) as fin
 				,a.respuesta
 				,a.timestamp
-				,(a.inicio
+				,(IFNULL(a.inicio,NOW())
 					+
 					INTERVAL (
 						TRUNCATE(
@@ -236,7 +285,8 @@ function sql_select_verifica_cron($data=array()){
 					) MINUTE
 				) as proxima_ejecucion
 				,NOW() as ahora
-				,TIMEDIFF(NOW(),
+				,IF(a.inicio IS NOT NULL,
+					TIMEDIFF(NOW(),
 					(a.inicio
 						+
 						INTERVAL (
@@ -247,8 +297,9 @@ function sql_select_verifica_cron($data=array()){
 							)
 						) MINUTE
 					)
-				) as excedido_tiempo
-				,TIME_TO_SEC(TIMEDIFF(NOW(),
+				),1) as excedido_tiempo
+				,IF(a.inicio IS NOT NULL,
+					TIME_TO_SEC(TIMEDIFF(NOW(),
 					(a.inicio
 						+
 						INTERVAL (
@@ -259,13 +310,13 @@ function sql_select_verifica_cron($data=array()){
 							)
 						) MINUTE
 					)
-				))/60 as excedido_minutos
-			,IF(NOW()>=CONCAT(b.inicio_fecha,' ',b.inicio_hora),1,0) as cron_iniciado
-			,IF(a.fin<=CONCAT(b.fin_fecha,' ',b.fin_hora),1,0) as cron_vigente
-			,b.activo as cron_activo
-			FROM (SELECT * FROM (SELECT * FROM cron_logs ORDER BY id_cronjob ASC, inicio DESC) AS tbl_unicos GROUP BY tbl_unicos.id_cronjob) AS a 
-			LEFT JOIN cron_tareas b ON a.id_cronjob=b.id_cronjob
-			WHERE 1 AND a.estatus='TERMINADO' $filtro
+				))/60,1) as excedido_minutos
+				,IF(NOW()>=CONCAT(b.inicio_fecha,' ',b.inicio_hora),1,0) as cron_iniciado
+				,IF(IFNULL(a.fin,NOW())<=CONCAT(b.fin_fecha,' ',b.fin_hora),1,0) as cron_vigente
+				,b.activo as cron_activo
+			FROM cron_tareas b
+			LEFT JOIN (SELECT * FROM (SELECT * FROM cron_logs ORDER BY id_cronjob ASC, inicio DESC) AS tbl_unicos GROUP BY tbl_unicos.id_cronjob) a ON a.id_cronjob=b.id_cronjob
+			WHERE 1 AND IFNULL(a.estatus,'TERMINADO')='TERMINADO' $filtro
 			;";
 	$resultado = SQLQuery($sql);
 	$resultado = (count($resultado)) ? $resultado : false ;
